4de5ab7f56d16cd174404aedf5d11317a822f4cd
hedi trabelsi <heditrabelsi412@gmail.com>
2026-04-03 02:06:59 +0100

    Add frontend

---
+++ FRONTEND/src/Controller/utilisateur/SecurityController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller\utilisateur;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    #[Route('/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // Si l'utilisateur est deja connecte, rediriger vers le tableau de bord
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_dashboard');
+        }
+
+        // Recuperer l'erreur de connexion s'il y en a une
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // Dernier email saisi par l'utilisateur
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('utilisateurs/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        // Cette methode est interceptee par le firewall de securite
+        throw new \LogicException('This method should never be called directly.');
+    }
+}
